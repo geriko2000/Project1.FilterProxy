95da1c1bae4eede73878d078beeff330cfafa73a
geriko2000 <geriko2000@yandex.ru>
2020-09-08 21:48:28 +0700

    ProxyGen, DAO, get, test table import

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,17 @@
 import static spark.Spark.*;
 
+import proxygen.Proxy;
 import proxygen.ProxyGen;
 import dao.*;
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String args[]) {
         //ProxyGen rp = new ProxyGen();
         //WriteProxyDAO.writeProxyInTable(rp.proxyGen(10));
 
-        get("/", (req, res) -> "hello there, write proxy");
+        /*get("/", (req, res) -> "hello there, write proxy");
         get("/proxy", (req, res) -> {
             String country = req.queryParams("country");
             String type = req.queryParams("type");
@@ -19,7 +22,10 @@ public class Main {
 
 
             return country + " " + type + " " + speed + " " + connecttimeout + " " + interval + " ok";
-        });
+        });*/
+
+        ArrayList<Proxy> test;
+        test = GetProxyDAO.getProxyFromTable();
 
     }
 
---
+++ src/main/java/dao/GetProxyDAO.java
@@ -0,0 +1,60 @@
+package dao;
+
+import proxygen.Proxy;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class GetProxyDAO {
+
+    public static ArrayList<Proxy> getProxyFromTable() {
+
+        try {
+
+            String DB_URL = "jdbc:postgresql://127.0.0.1:5432/MyData";
+            String USER = "postgres";
+            String PASS = "1623";
+
+            Class.forName("org.postgresql.Driver");
+
+            try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS)) {
+
+                ArrayList<Proxy> proxylist = new ArrayList<Proxy>();
+
+                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM proxies");
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()){
+
+                    Proxy newProxy = new Proxy();
+                    newProxy.country = resultSet.getString("country");
+                    newProxy.host = resultSet.getString("host");
+                    newProxy.type = resultSet.getString("type");
+                    Timestamp timestamp = resultSet.getTimestamp("timestamp");
+                    newProxy.timestamp = timestamp.toLocalDateTime();
+                    newProxy.port = resultSet.getInt("port");
+                    newProxy.connecttimeout = resultSet.getInt("connecttimeout");
+                    newProxy.speed = resultSet.getInt("speed");
+
+                    proxylist.add(newProxy);
+
+                    System.out.println(newProxy.country);
+                }
+
+                System.out.println("Table imported successfully");
+                return proxylist;
+
+            } catch (Exception e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Connection failed...");
+            System.out.println(ex);
+
+        }
+
+        return null;
+    }
+}
